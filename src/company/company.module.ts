d9d9d0f1806e6086fa98447aed2e555ed3235b26
yupimaru-sikaku <yukiharu.nagao@icloud.com>
2022-07-30 19:17:14 +0900

    ⚡️TypeOrmをUser, Compnay, Adminで使用

--- src/company/company.module.ts
+++ src/company/company.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { CompanyController } from './company.controller';
 import { CompanyService } from './company.service';
+import { Company } from './compnay';
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([Company])
+  ],
   controllers: [CompanyController],
   providers: [CompanyService]
 })
