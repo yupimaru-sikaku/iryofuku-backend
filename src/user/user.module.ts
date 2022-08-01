e5a767fa9316b2b69de8271b68802c886cc6690d
yupimaru-sikaku <yukiharu.nagao@icloud.com>
2022-08-01 11:42:34 +0900

    ⚡️JWT使用

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,14 +1,17 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { SharedModlue } from 'src/shared/shared.module';
 import { User } from './user';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([User])
+    TypeOrmModule.forFeature([User]),
+    SharedModlue
   ],
   controllers: [UserController],
-  providers: [UserService]
+  providers: [UserService],
+  exports: [UserService]
 })
 export class UserModule {}
