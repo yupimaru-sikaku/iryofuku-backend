6a3d815b8950da94366603529dbc468dc661a5e3
yupimaru-sikaku <yukiharu.nagao@icloud.com>
2022-08-01 10:27:17 +0900

    ⚡️共通CRUD操作追加

---
+++ src/shared/abstract.service.ts
@@ -0,0 +1,27 @@
+import { Repository } from "typeorm";
+
+export abstract class AbstractService {
+    protected constructor(
+        protected readonly repository: Repository<any>
+    ){}
+
+    async save(options) {
+        return this.repository.save(options);
+    }
+
+    async find(options = {}) {
+        return this.repository.find(options);
+    }
+
+    async findOne(options) {
+        return this.repository.findOne(options);
+    }
+
+    async updata(id: string, options) {
+        return this.repository.update(id, options);
+    }
+
+    async delete(id: string) {
+        return this.repository.delete(id);
+    }
+}
