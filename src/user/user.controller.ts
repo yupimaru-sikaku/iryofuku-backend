a71ff52993ad3ad4dfcd0b78be70b9a97426e0b3
yupimaru-sikaku <yukiharu.nagao@icloud.com>
2022-08-02 09:07:41 +0900

    ⚡️UserのCRUD作成

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,20 +1,26 @@
 import {
   BadRequestException,
   Body,
+  ClassSerializerInterceptor,
   Controller,
   Get,
   NotFoundException,
   Post,
+  Put,
+  Req,
   Res,
+  UseGuards,
+  UseInterceptors,
 } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { RegisterDto } from './dtoList/register.dto';
 import { UserService } from './user.service';
 import * as bcrypt from 'bcryptjs';
-import { Response } from 'express';
-import { v4 as uuidV4 } from 'uuid';
+import { Response, Request } from 'express';
+import { JwtAuthGuard } from 'src/shared/jwt-auth.guard';
 
 @Controller()
+@UseInterceptors(ClassSerializerInterceptor)
 export class UserController {
   constructor(
     private userService: UserService,
@@ -29,13 +35,10 @@ export class UserController {
 
     const { password_confirm, ...data } = body;
 
-    const uuid = uuidV4()
-
     const hashed = await bcrypt.hash(body.password, 12);
 
     return this.userService.save({
       ...data,
-      id: uuid,
       password: hashed,
       is_user: true,
       is_admin: false,
@@ -69,5 +72,78 @@ export class UserController {
     return {
       message: 'ログインしました',
     };
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Get('user')
+  async user(@Req() request: Request) {
+    const cookie = request.cookies['jwt'];
+
+    const { id } = await this.jwtService.verifyAsync(cookie);
+
+    const user = await this.userService.findOne({ where: { id } });
+
+    const { password, ...data } = user;
+
+    return {
+      ...data,
+    };
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Post('user/logout')
+  async logout(@Res({ passthrough: true }) response: Response) {
+    response.clearCookie('jwt');
+
+    return {
+      message: 'ログアウトしました',
+    };
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Put('user/info')
+  async updateInfo(
+    @Req() request: Request,
+    @Body('office_name') office_name: string,
+    @Body('postal_code') postal_code: string,
+    @Body('address') address: string,
+    @Body('personal_name') personal_name: string,
+    @Body('phone_number') phone_number: string,
+  ) {
+    const cookie = request.cookies['jwt'];
+
+    const { id } = await this.jwtService.verifyAsync(cookie);
+
+    await this.userService.update(id, {
+      office_name,
+      postal_code,
+      address,
+      personal_name,
+      phone_number,
+    });
+
+    return this.userService.findOne({ where: { id } });
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Put('user/password')
+  async updatePassword(
+    @Req() request: Request,
+    @Body('password') password: string,
+    @Body('password_confirm') password_confirm: string,
+  ) {
+    if (password !== password_confirm) {
+      throw new BadRequestException('パスワードが一致しません');
+    }
+
+    const cookie = request.cookies['jwt'];
+
+    const { id } = await this.jwtService.verifyAsync(cookie);
+
+    await this.userService.update(id, {
+      password: await bcrypt.hash(password, 12),
+    });
+
+    return this.userService.findOne({ where: { id } });
   }
 }
