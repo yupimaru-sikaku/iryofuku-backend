e01ecde04c8b24b137ecd4f9eeeccbff6cb25193
yupimaru-sikaku <yukiharu.nagao@icloud.com>
2022-08-02 10:13:03 +0900

    ♻️リファクタリング

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,14 +1,14 @@
 import { Injectable } from '@nestjs/common';
-import { InjectRepository } from "@nestjs/typeorm";
-import { User } from "./user";
-import { Repository } from "typeorm";
-import { AbstractService } from "../shared/abstract.service";
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from './user';
+import { Repository } from 'typeorm';
+import { AbstractService } from '../shared/abstract.service';
 
 @Injectable()
 export class UserService extends AbstractService {
-    constructor(
-        @InjectRepository(User) private readonly userRepository: Repository<User>
-    ) {
-        super(userRepository);
-    }
+  constructor(
+    @InjectRepository(User) private readonly userRepository: Repository<User>,
+  ) {
+    super(userRepository);
+  }
 }
